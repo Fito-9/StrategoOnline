660bf8945898be3d2b85721063bc9362163df9fa
gruiazu <gruiazu@g.educaand.es>
2025-01-16 19:32:37 +0100

    main hecho

--- StrategoAngular/src/app/app.routes.ts
+++ StrategoAngular/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './pages/header/header.component';
+import { MainComponent } from './pages/main/main.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'header',
+        component: HeaderComponent
+    },
+    {
+        path:'',
+        component: MainComponent
+    }
+];
