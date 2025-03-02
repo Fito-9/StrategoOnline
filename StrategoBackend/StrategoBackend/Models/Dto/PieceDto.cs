2372587b36cbc817f2f4f60af132b3aa96238052
Fito-9 <aburbel032@g.educaand.es>
2025-03-02 23:24:43 +0100

    ministage

---
+++ StrategoBackend/StrategoBackend/Models/Dto/PieceDto.cs
@@ -0,0 +1,11 @@
+﻿/*// Models/Dto/PieceDto.cs
+namespace StrategoBackend.Models.Dto
+{
+    public class PieceDto
+    {
+        public int Row { get; set; }
+        public int Col { get; set; }
+        public string Name { get; set; }
+    }
+}
+*/
